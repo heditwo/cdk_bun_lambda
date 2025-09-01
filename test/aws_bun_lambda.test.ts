94e7f6ffe139cd6eb990d2e9457cc90e571645f5
heditwo <45843237+heditwo@users.noreply.github.com>
2025-09-01 17:28:49 -0400

    Initial commit

---
+++ test/aws_bun_lambda.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsBunLambda from '../lib/aws_bun_lambda-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws_bun_lambda-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsBunLambda.AwsBunLambdaStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
